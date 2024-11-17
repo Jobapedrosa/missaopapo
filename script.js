27bd11131bb5c42f803ccad4c70e28ce50d0ab18
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2024-11-17 13:54:09 +0000

    alt

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Como você acredita que podemos combater o racismo na sociedade?",
+        enunciado: "Como Você Acredita que Podemos Combater o Racismo na Sociedade?",
         alternativas: [           
             {
                 texto: "Participando de campanhas de conscientização e educação sobre igualdade racial.",
